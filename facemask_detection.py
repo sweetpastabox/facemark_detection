0a699c2e2b60df1fc032fae8917b69576d530df5
sweetpastabox <66618339+sweetpastabox@users.noreply.github.com>
2021-11-27 20:28:50 +0100

    Update facemask_detection.py

--- facemask_detection.py
+++ facemask_detection.py
@@ -41,9 +41,9 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 	return(locs, preds)
 
-prototxPath = r"face_detector/deploy.prototxt"
+prototxtPath = r"face_detector/deploy.prototxt"
 weightsPath = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
-faceNet = cv2.dnn.readNet(prototxPath, weightsPath)
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 maskNet = load_model("mask_detector.model")
 
