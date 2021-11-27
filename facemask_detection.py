8b32394628232a6e07a55c47cabb0e515dc21ecc
sweetpastabox <66618339+sweetpastabox@users.noreply.github.com>
2021-11-27 17:42:47 +0100

    Update facemask_detection.py

--- facemask_detection.py
+++ facemask_detection.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.application.mobilenet_v2 import preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
